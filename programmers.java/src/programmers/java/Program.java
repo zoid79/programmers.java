46c0e6b7eaf89f4eeba320754c8f4b9682458d7d
zoid79 <zidwkd00@naver.com>
2023-07-03 23:11:57 +0900

    level 2 JadenCase

--- programmers.java/src/programmers/java/Program.java
+++ programmers.java/src/programmers/java/Program.java
@@ -7,43 +7,27 @@ import java.util.Vector;
 public class Program {
 
 	public static void main(String[] args) {
-		int[] arr = {1,2,4,3,5}; 
-		System.out.println(Solution.solution(arr));
+		System.out.println(Solution.solution("        "));
 	}
 	class Solution {
-	    public static int solution(int[] order) {
-	    	Stack sub=new Stack();
-	        int answer = 0;
-	        int start=order[0];
-	        int count=2;
-	    	for(int i=1;i<start;i++) {
-	    		sub.push(i);
-	    		count++;
-	    	}
-    		answer++;
-	    	for(int i=1;i<order.length;i++) {
-	    		if(sub.size()>0&&sub.peek().equals(order[i])) {
-	    			sub.pop();
-	    			answer++;
-	    			continue;
-	    		}
-	    		else if(order[i]==count) {
-	    			answer++;
-	    			count++;
-	    			continue;
-	    		}else if(order[i]>count) {
-	    			while(order[i]!=count) {
-	    	    		sub.push(count);
-	    	    		count++;
-	    	    	}
-	    			System.out.println(count);
-	    			answer++;
-	    			continue;
-	    		}
-	    		else
-	    		break;
-	    	}
-	        return answer;
+	    public static String solution(String s) {
+	        String answer = s;
+	        String answer2="";
+	        char[] arrayanswerarray = answer.toCharArray();
+	        int first=0;
+	        for(int i=0;i<arrayanswerarray.length;i++) {
+	    //    	System.out.println();
+	        	if(first==0&&arrayanswerarray[i]>='a'&&arrayanswerarray[i]<='z') {
+	        		arrayanswerarray[i]=(char) (arrayanswerarray[i]-32);
+	        	}else if(first!=0&&arrayanswerarray[i]>='A'&&arrayanswerarray[i]<='Z') {
+	        		arrayanswerarray[i]=(char) (arrayanswerarray[i]+32);
+	        	}else if(arrayanswerarray[i]==' ')
+	        		first=-1;
+	        	first++;
+	        }
+	        for(char c:arrayanswerarray)
+	        	answer2=answer2+c;
+	        return answer2;
 	    }
 	}
-}
+	}
